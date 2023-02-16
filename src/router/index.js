002442c3d4f554e21e90ce14e3a12afdf57335de
Zhangchaopei <qq1090015989@gmail.com>
2023-02-16 20:15:48 +0800

    ==

--- src/router/index.js
+++ src/router/index.js
@@ -44,7 +44,7 @@ export const constantRoutes = [
     redirect: '/dashboard',
     children: [{
       path: '/dashboard',
-      name: 'dashboard',
+      name: 'Dashboard',
       component: () => import('@/views/dashboard/index'),
       meta: { title: '首页', icon: 'dashboard' }
     }]
