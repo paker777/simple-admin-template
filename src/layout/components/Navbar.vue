4a8a02a1d6613f3bae55532b28b54ad2928b971e
Zhangchaopei <1090015989@qq.com>
2023-02-17 15:40:28 +0800

    ==

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -7,7 +7,7 @@
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img src="@/assets/common/head.jpg" class="user-avatar">
+          <img src="@/assets/common/head.gif" class="user-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
